104253c126bf3c528cb560f8d34170e60432380d
Robin Nabel <robin.nabel@gmail.com>
2016-04-21 17:54:49 +0100

    Sugar chart added - not tested.

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,66 @@ var globalData = null,
     filteredDataset = null,
     filteredData = null,
     mapContainer = $("#country-chooser");
-var geoChart = dc.geoChoroplethChart("#country-chooser");
+var geoChart = dc.geoChoroplethChart("#country-chooser"),
+    fatChart = dc.lineChart("#fat-amount"),
+    sugarChart = dc.lineChart("#sugar-amount");
 
 var width = parseFloat(mapContainer.css('width').replace("px", "")),
     height = 500;
+
+var setupWorldMap = function (countryDimension, countryGrp, world_countries) {
+    var projection = d3.geo.mercator()
+        // .center([-100,0])
+        .scale(200)
+        .translate([width / 2, height]);
+
+    function zoomed() {
+        projection
+            .translate(d3.event.translate)
+            .scale(d3.event.scale);
+        geoChart.render();
+    }
+
+    var zoom = d3.behavior.zoom()
+        .translate(projection.translate())
+        .scale(projection.scale())
+        .scaleExtent([height / 2, 8 * height])
+        .on("zoom", zoomed);
+
+    var svg = d3.select("#country-chooser")
+        // .attr("width", 1000)
+        // .attr("height", 400)
+        .call(zoom);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    geoChart
+        .projection(projection)
+        .width(parseFloat(mapContainer.css('width').replace("px", "")))
+        .height(600)
+        .transitionDuration(1000)
+        .dimension(countryDimension)
+        .group(countryGrp)
+        .colors(d3.scale.linear()
+            .domain([0, 400, 10000])
+            .range(["#e0f2f1", "#26a69a", "#004d40"]))
+        .colorCalculator(function (d) {
+            var colorToReturn;
+            if (d) {
+                colorToReturn = geoChart.colors()(d);
+            } else {
+                colorToReturn = "#F5F2F0"
+            }
+            return colorToReturn;
+        })
+        .overlayGeoJson(world_countries.features, "countries", function (d) {
+            return d.properties.name;
+        })
+        .title(function (d) {
+            return "Country: " + d.key + " " + (d.value ? d.value : 0) + " Products";
+        });
+};
 
 d3.json("../data/world-countries.json", function (error, world_countries) {
     d3.csv("../data/world-food-facts/FoodFacts_filtered.csv", function (data) {
@@ -21,60 +77,21 @@ d3.json("../data/world-countries.json", function (error, world_countries) {
 
         var countryGrp = countryDimension.group().reduceCount();
 
-        console.log(countryDimension.top(10));
-        console.log(countryGrp.all().length);
+        setupWorldMap(countryDimension, countryGrp, world_countries);
 
-        var projection = d3.geo.mercator()
-            // .center([-100,0])
-            .scale(200)
-            .translate([width / 2, height]);
+        var sugarDimension = crsData.dimension(function (data) {
+           return data.sugars_100g;
+        });
 
-        function zoomed() {
-            projection
-                .translate(d3.event.translate)
-                .scale(d3.event.scale);
-            geoChart.render();
-        }
+        var sugarGroup = sugarDimension.group().reduceCount();
 
-        var zoom = d3.behavior.zoom()
-            .translate(projection.translate())
-            .scale(projection.scale())
-            .scaleExtent([height / 2, 8 * height])
-            .on("zoom", zoomed);
-
-        var svg = d3.select("#country-chooser")
-            // .attr("width", 1000)
-            // .attr("height", 400)
-            .call(zoom);
-
-        var path = d3.geo.path()
-            .projection(projection);
-
-        geoChart
-            .projection(projection)
-            .width(parseFloat(mapContainer.css('width').replace("px", "")))
-            .height(600)
+        // Set up fat and sugar line charts.
+        sugarChart
+            .width("100%")
+            .height(300)
             .transitionDuration(1000)
             .dimension(countryDimension)
-            .group(countryGrp)
-            .colors(d3.scale.linear()
-                .domain([0, 400, 10000])
-                .range(["#e0f2f1", "#26a69a", "#004d40"]))
-            .colorCalculator(function (d) {
-                var colorToReturn;
-                if (d) {
-                    colorToReturn = geoChart.colors()(d);
-                } else {
-                    colorToReturn = "#F5F2F0"
-                }
-                return colorToReturn;
-            })
-            .overlayGeoJson(world_countries.features, "countries", function (d) {
-                return d.properties.name;
-            })
-            .title(function (d) {
-                return "Country: " + d.key + " " + (d.value ? d.value : 0) + " Products";
-            });
+            .group(sugarGroup);
 
         dc.renderAll();
         console.log("Finished loading.");
